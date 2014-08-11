94a95171e5c5ccff8d25c8624b6900c1ea0fceab
Sandy Ludosky <sandyludosky@gmail.com>
2014-08-10 20:01:25 -0400

    ui refinement

---
+++ js/resume.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+     setInterval(function() {
+     	$('.header').fadeIn()
+	}, 1000);
+
+     setInterval(function() {
+     	$('.biography').fadeIn()
+	}, 1500);
+
+    setInterval(function() {
+        $('.skills').fadeIn()
+    }, 2000);
+
+
+    setInterval(function() {
+        $('.projects').fadeIn()
+    }, 2500);
+});
---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+	//random bg image
+	var numRand = Math.floor(Math.random()*5)
+	$('style').html("body.f-topbar-fixed {background: url('img/"+ numRand +".png');}")
+
+	//setting height equal to window's height
+	var height = $(window).height();
+	var height_65 = height - 65
+	var height_3 = height *  31/100
+
+	$('#welcome').css('height', height);
+	$('#work').css('height', height);
+	$('#inner').css('height', height_65);
+
+	// $('#contact').css('height', height);
+
+
+
+    $('.diamond').on('mouseenter', function(){
+    	console.log('hover');
+
+    	$(this).find('.diamond_hover').fadeIn('slow');
+    	$(this).find('.mask').fadeIn('slow');
+
+    });
+
+     $('.diamond').on('mouseleave', function(){
+    	console.log('leave');
+    	$(this).find('.diamond_hover').fadeOut('slow');
+    	$(this).find('.mask').fadeOut('slow');
+
+    });
+
+     setInterval(function() {
+     	$('#inner').fadeIn()
+	}, 1000);
+
+     setInterval(function() {
+     	$('#bottom').fadeIn()
+	}, 2000);
+
+
+});
