9fe75f87e4a9d50566413b2770d96cb71d7bd8b0
Sandy L <sandyl@Sandys-MacBook-Pro.local>
2015-12-06 19:32:24 -0500

    site in construction

--- js/script.js
+++ js/script.js
@@ -1,51 +1,18 @@
-
- $(document).ready(function() {
+$(document).ready(function() {
 	 
 	  // to use the window height
-	 var height = $(window).height();
-	 var width = $(window).width();
-   var window_height = (height*3)+250;
+	 var height = $(window).innerHeight() - 42;
 
-    console.log(window_height);
+   console.log(height);
 
-   $('#home, #contact').css('height', height);
+   $('#home').css('height', height);
 
-   $('.portfolio-item').on('mouseenter', function(){
-          $(this).find('.item_title').fadeIn('slow');
-   });
+   setInterval(function() {
 
-    $('.portfolio-item').on('mouseleave', function(){
-          $(this).find('.item_title').fadeOut('slow');
-  
-   });
+    $('.inConstruction')  
+        .fadeIn('slow')
+        .addClass('bounceInLeft')        
+    }, 1500);
 
-    $('#udemy').css('top',window_height);	
-	
- 
-	   
-	   
-	 
-	
-	
-  $(window).scroll(function () {    
-
-    var scroll = $(window).scrollTop();
-    if (scroll>150) {
-  	$('nav').fadeIn('slow');
-    } else {
-  	$('nav').fadeOut('slow');
-    }
-  });
-
-   // jQuery for page scrolling feature - requires jQuery Easing plugin
-   $(function() {
-      $('.page-scroll a').bind('click', function(event) {
-          var $anchor = $(this);
-          $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
-          }, 1000, 'easeInOutExpo');
-          event.preventDefault();
-      });
-  });
 
 });
